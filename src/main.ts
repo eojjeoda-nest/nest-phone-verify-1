2aba8ad713176e9c1684bdce95a944d0805d1286
ppinkypeach <mira20932@gmail.com>
2023-11-21 20:38:19 +0900

    refactor: 전화번호 형식 유효성체크

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -17,6 +18,12 @@ async function bootstrap() {
 
   const swagger = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, swagger);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+  }));
 
   await app.listen(process.env.PORT || 8000);
 
