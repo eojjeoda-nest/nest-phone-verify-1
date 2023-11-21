6be22d692a95f3167e501f9fc568a79f5301b73c
jiminsong490 <misosjm@naver.com>
2023-11-21 21:28:45 +0900

    feat : dto 유효성 검사 설정

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -8,6 +9,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // TODO: 프로그램 구현
   await app.listen(process.env.PORT || 8000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
