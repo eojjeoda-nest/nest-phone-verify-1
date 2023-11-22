428a4062fcfd2cf1041638d71893e19c0c6c28de
ssojungg <kawy.sojung@gmail.com>
2023-11-23 03:21:02 +0900

    feat: dto 수정

--- src/notificaiton/dto/notificaiton-response.dto.ts
+++ src/notificaiton/dto/notificaiton-response.dto.ts
@@ -1,11 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
-
-import { IsNotEmpty, IsString, Length, Matches } from "class-validator";
+import { IsNotEmpty, IsString, Length } from "class-validator";
 
 export class CheckCodeDto {
   @IsNotEmpty()
   @IsString()
   @Length(6, 6)
-  @ApiProperty({ example: '123456', description: 'Verification code' })
+  @ApiProperty({ example: '123456', description: '인증 코드' })
   code: string;
 }
