5e9227026bc49b1dfa9b4ef41b108e72ab31b25a
jiminsong490 <misosjm@naver.com>
2023-11-24 21:36:51 +0900

    remove : 사용하지 않는 domain 삭제

--- src/domain/auth-codes/auth-codes.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { AuthCodesService } from './auth-codes.service';
-
-@Controller('auth-codes')
-export class AuthCodesController {
-  constructor(private readonly authCodesService: AuthCodesService) {}
-
-}
--- src/domain/auth-codes/auth-codes.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthCodesService } from './auth-codes.service';
-import { AuthCodesController } from './auth-codes.controller';
-
-@Module({
-  controllers: [AuthCodesController],
-  providers: [AuthCodesService],
-})
-export class AuthCodesModule {}
