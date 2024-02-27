5654a0acf2bf0ea3339a88fceed8303aebc1eb86
hangyeol <snake011128@naver.com>
2024-02-27 15:59:11 +0900

    feat: module 추가

---
+++ src/modules/phone/phone.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Phone } from './entities/phone.entity';
+import { PhoneController } from './phone.controller';
+import { PhoneService } from './phone.service';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Phone])],
+  controllers: [PhoneController],
+  providers: [PhoneService],
+})
+export class PhoneModule {}
