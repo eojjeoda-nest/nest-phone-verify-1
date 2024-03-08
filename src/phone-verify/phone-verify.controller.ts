edaa7fa22031c5a6903c45405d21555d8e1f9a35
sangminlee98 <tkdals0975@naver.com>
2024-03-08 22:10:47 +0900

    feat : swagger 설정

--- src/phone-verify/phone-verify.controller.ts
+++ src/phone-verify/phone-verify.controller.ts
@@ -4,11 +4,16 @@ import { SendCodeRequestDto } from './dto/request/send-code-request.dto';
 import { SendCodeResponseDto } from './dto/response/send-code-response.dto';
 import { VerifyCodeRequestDto } from './dto/request/verify-code-request.dto';
 import { VerifyCodeResponseDto } from './dto/response/verify-code-response.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller('phone-verify')
+@ApiTags('phone-verify')
 export class PhoneVerifyController {
   constructor(private readonly phoneVerifyService: PhoneVerifyService) {}
 
+  @ApiOperation({
+    summary: '휴대폰 인증번호 발송',
+  })
   @Post('send-code')
   async sendVerifyCode(
     @Body() dto: SendCodeRequestDto,
@@ -16,6 +21,9 @@ export class PhoneVerifyController {
     return this.phoneVerifyService.sendCode(dto);
   }
 
+  @ApiOperation({
+    summary: '휴대폰 인증',
+  })
   @Patch('/verify-code')
   verify(@Body() dto: VerifyCodeRequestDto): Promise<VerifyCodeResponseDto> {
     return this.phoneVerifyService.verifyCode(dto);
